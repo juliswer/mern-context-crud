61ea4fa25bbc900e1f718857ddc360664cb41e49
juliswer <julianswer36@gmail.com>
2022-04-12 17:08:12 -0300

    making the order

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
-import express from "express";
-import postRoutes from "./routes/posts.routes.js";
 import { connectDB } from "./db.js";
 import { PORT } from "./config.js";
+import app from "./app.js";
 
-const app = express();
 connectDB();
-
-app.use(postRoutes);
 
 app.listen(PORT);
 console.log("Server is running on port", PORT);
