4b704fa7e50473a88470fd8e7e5cd57ec442c469
juliswer <julianswer36@gmail.com>
2022-04-12 17:03:04 -0300

    updating env, controllers and db connection

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT;
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/posts-mern-crud';
+export const PORT = process.env.PORT || 4000;
