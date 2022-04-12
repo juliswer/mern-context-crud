1799cd89523d882188ca968a2dd7393f95e0122a
juliswer <julianswer36@gmail.com>
2022-04-12 17:01:15 -0300

    updating env, controllers and db connection

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config();
+import {MONGODB_URI} from './config.js'
 
 export async function connectDB() {
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI);
+    const db = await mongoose.connect(MONGODB_URI);
     console.log("Connected to ", db.connection.name);
   } catch (error) {
     console.log(error);
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from "express";
-import postRoutes from './routes/posts.routes.js';
-import {connectDB} from './db.js'
+import postRoutes from "./routes/posts.routes.js";
+import { connectDB } from "./db.js";
+import { PORT } from "./config.js";
 
 const app = express();
-connectDB()
+connectDB();
 
-app.use(postRoutes)
+app.use(postRoutes);
 
-app.listen(4000);
-console.log("Server is running on port", 4000);
+app.listen(PORT);
+console.log("Server is running on port", PORT);
