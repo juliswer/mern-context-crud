34aeef51a2fb67cee63b2fc5c5eecd2acdcd1b41
juliswer <julianswer36@gmail.com>
2022-04-13 20:53:16 -0300

    updating context

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import { usePosts } from "../context/postContext";
 
 export function HomePage() {
-  const context = usePosts();
-  console.log(context);
+  const {posts} = usePosts();
+  console.log(posts);
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+        </div>
+      ))}
     </div>
   );
 }
