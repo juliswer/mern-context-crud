02775a385459f562418c7edb614ab0c0b65b08fe
juliswer <julianswer36@gmail.com>
2022-04-13 17:46:27 -0300

    uploading tailwinds

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { HomePage, PostForm, NotFoundPage } from "./pages";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/new" element={<PostForm />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/new" element={<PostForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
