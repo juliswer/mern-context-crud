7bf954641658fd6f955d6f9a3f43b5c7eb81eb8a
juliswer <julianswer36@gmail.com>
2022-04-14 11:05:39 -0300

    uploading the context for upload task

--- client/src/pages/PostForm.jsx
+++ client/src/pages/PostForm.jsx
@@ -1,10 +1,10 @@
-import {usePosts} from '../context/postContext';
-import { Formik, Form, Field } from "formik";
-import {useNavigate} from 'react-router-dom';
+import { usePosts } from "../context/postContext";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 export function PostForm() {
-
-  const {createPost} = usePosts();
+  const { createPost } = usePosts();
   const navigate = useNavigate();
 
   return (
@@ -14,15 +14,21 @@ export function PostForm() {
           title: "",
           description: "",
         }}
+        validationSchema={Yup.object({
+          title: Yup.string().required("Title is required"),
+          description: Yup.string().required("Description is required"),
+        })}
         onSubmit={async (values, actions) => {
-          await createPost(values)
-          navigate('/')
+          await createPost(values);
+          navigate("/");
         }}
       >
-        {({handleSubmit}) => (
+        {({ handleSubmit }) => (
           <Form onSubmit={handleSubmit}>
-            <Field name="title" placeholder="title" />
-            <Field name="description" placeholder="description" />
+            <Field name="title" placeholder="title" className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full" />
+            <ErrorMessage component="p" className="text-red-400 text-sm" name="title" />
+            <Field name="description" placeholder="description" className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full" />
+            <ErrorMessage component="p" name="description" className="text-red-400 text-sm" />
             <button type="submit">Save</button>
           </Form>
         )}
