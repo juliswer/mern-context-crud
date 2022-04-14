f89563102c9c6b2f1ca11c63617c74f8504fe2da
juliswer <julianswer36@gmail.com>
2022-04-13 21:00:21 -0300

    updating context

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,3 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
+// const PRODUCTION_URL = process.env.REACT_APP_PRODUCTION_URL;
 
-export const getPostsRequest = async () => await axios.get('http://localhost:4000/posts')
+// NOTE: IF YOU ARE DEPLOYING THIS APP, YOU MUST MAKE THE PRODUCTION_URL IN THE .ENV FILE. THEN YOU PASS IT TO THE AXIOS FUNCTION.
+
+export const getPostsRequest = async () =>
+  await axios.get("http://localhost:4000/posts");
