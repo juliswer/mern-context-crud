9f04106712cf0aa18e7427db76787b588ced84af
juliswer <julianswer36@gmail.com>
2022-04-14 11:53:36 -0300

    deleteFunction

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -10,7 +10,10 @@ function PostCard({ post }) {
       <div>
         <p className="text-white">Are you sure you want to delete this post? <strong>{id}</strong></p>
         <div>
-          <button className="bg-red-500 hover:bg-red-400 px-3 py-2 text-sm text-white rounded-sm mx-2" onClick={() => deletePost(id)}>Delete</button>
+          <button className="bg-red-500 hover:bg-red-400 px-3 py-2 text-sm text-white rounded-sm mx-2" onClick={() => {
+            deletePost(id);
+            toast.dismiss(t.id)
+          }}>Delete</button>
           <button className="bg-slate-400 hover:bg-slate-500 px-3 py-2 text-white rounded-sm mx-2" onClick={() => toast.dismiss(t.id)}>
             Cancel
           </button>
