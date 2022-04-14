2616f68e8e41738532e0a8e02640d877587b1c22
juliswer <julianswer36@gmail.com>
2022-04-14 13:16:20 -0300

    update task form

--- client/src/pages/PostForm.jsx
+++ client/src/pages/PostForm.jsx
@@ -17,51 +17,72 @@ export function PostForm() {
     (async () => {
       if (params.id) {
         const res = await getPost(params.id);
-        setPost(res)
+        setPost(res);
       }
     })();
   }, [getPost, params.id]);
 
   return (
-    <div>
-      <Formik
-        initialValues={post}
-        validationSchema={Yup.object({
-          title: Yup.string().required("Title is required"),
-          description: Yup.string().required("Description is required"),
-        })}
-        onSubmit={async (values, actions) => {
-          await createPost(values);
-          navigate("/");
-        }}
-        enableReinitialize={true}
-      >
-        {({ handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
-            <Field
-              name="title"
-              placeholder="title"
-              className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full"
-            />
-            <ErrorMessage
-              component="p"
-              className="text-red-400 text-sm"
-              name="title"
-            />
-            <Field
-              name="description"
-              placeholder="description"
-              className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full"
-            />
-            <ErrorMessage
-              component="p"
-              name="description"
-              className="text-red-400 text-sm"
-            />
-            <button type="submit">Save</button>
-          </Form>
-        )}
-      </Formik>
+    <div className="flex items-center justify-center">
+      <div className="bg-zinc-800 p-10 shadow-md shadow-black rounded-xl">
+        <Formik
+          initialValues={post}
+          validationSchema={Yup.object({
+            title: Yup.string().required("Title is required"),
+            description: Yup.string().required("Description is required"),
+          })}
+          onSubmit={async (values, actions) => {
+            await createPost(values);
+            navigate("/");
+          }}
+          enableReinitialize={true}
+        >
+          {({ handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              <label
+                htmlFor="title"
+                className="text-sm block font-bold text-gray-400"
+              >
+                Title
+              </label>
+              <Field
+                name="title"
+                placeholder="title"
+                className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full"
+              />
+              <ErrorMessage
+                component="p"
+                className="text-red-400 text-sm"
+                name="title"
+              />
+              <label
+                htmlFor="description"
+                className="text-sm block font-bold text-gray-400"
+              >
+                Description
+              </label>
+              <Field
+                name="description"
+                component="textarea"
+                placeholder="description"
+                rows={3}
+                className="px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full"
+              />
+              <ErrorMessage
+                component="p"
+                name="description"
+                className="text-red-400 text-sm"
+              />
+              <button
+                type="submit"
+                className="bg-indigo-600 hover:bg-indigo-500 px-4 py-2 rounded mt-2 text-white focus:outline-none disabled:bg-indigo-400"
+              >
+                {params.id ? "Update" : "Save"}
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
