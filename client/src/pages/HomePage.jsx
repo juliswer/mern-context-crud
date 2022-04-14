5ac9aa75e9a65ee449d2d54af95eec8ad8b430c2
juliswer <julianswer36@gmail.com>
2022-04-14 16:13:20 -0300

    fixing upload image error

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { usePosts } from "../context/postContext";
 import { VscEmptyWindow } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import PostCard from "../components/PostCard";
-import {AiOutlineLink} from 'react-icons/ai';
+import { AiOutlineLink } from "react-icons/ai";
 
 export function HomePage() {
   const { posts } = usePosts();
@@ -12,18 +12,26 @@ export function HomePage() {
       <div className="flex flex-col justify-center items-center">
         <VscEmptyWindow className="w-48 h-48 text-white" />
         <h1 className="text-white text-2xl">
-          There are no posts yet. <Link to="/new" className="text-blue-500 flex">Start creating one!<AiOutlineLink className="ml-2" /></Link>
+          There are no posts yet.{" "}
+          <Link to="/new" className="text-blue-500 flex">
+            Start creating one!
+            <AiOutlineLink className="ml-2" />
+          </Link>
         </h1>
       </div>
     );
 
   return (
     <div className="text-white">
-      <h1 className="text-3xl font-bold">Home Page ({posts.length} Posts) </h1>
-      <Link to="/new">Create New Post</Link>
+      <header className="flex justify-between py-4">
+        <h1 className="text-3xl font-bold">
+          Posts ({posts.length} {posts.length === 1 ? "Post" : "Posts"})
+        </h1>
+        <Link to="/new">Create New Post</Link>
+      </header>
       <div className="grid grid-cols-3 gap-2">
         {posts.map((post) => (
-            <PostCard key={post._id} post={post} />
+          <PostCard key={post._id} post={post} />
         ))}
       </div>
     </div>
