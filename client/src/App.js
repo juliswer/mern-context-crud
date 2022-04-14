9ec4003625391a3b7fd8b912e21ca55861d9af57
juliswer <julianswer36@gmail.com>
2022-04-14 11:17:55 -0300

    postcard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { HomePage, PostForm, NotFoundPage } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import { PostProvider } from "./context/postContext";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="bg-neutral-900 min-h-screen flex items-center">
+    <div className="bg-neutral-900 min-h-screen flex items-center justify-center">
       <div className="px-10 container">
         <PostProvider>
           <Routes>
@@ -12,6 +13,7 @@ function App() {
             <Route path="/new" element={<PostForm />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
+          <Toaster />
         </PostProvider>
       </div>
     </div>
--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,9 +1,16 @@
+import toast from 'react-hot-toast';
+
 function PostCard({ post }) {
   return (
     <div className="bg-zinc-800 text-white rounded-md shadow-black hover:bg-zinc-700 hover:cursor-pointer">
       <div className="px-4 py-7">
-        <h3>{post.title}</h3>
-        <h3>{post.description}</h3>
+        <div className="flex justify-between">
+          <p>{post.title}</p>
+          <button className="bg-red-600 text-sm px-2 py-1 rounded-sm" onClick={() => toast.success('hello world')}>
+            Delete
+          </button>
+        </div>
+        <p>{post.description}</p>
       </div>
     </div>
   );
