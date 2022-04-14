2671c54a45bc17e4539a40be7ce5563bdc5391da
juliswer <julianswer36@gmail.com>
2022-04-14 16:49:12 -0300

    deploy

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ import express from "express";
 import fileUpload from 'express-fileupload';
 import postRoutes from "./routes/posts.routes.js";
 import cors from 'cors'
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // MIDDLEWARE
 app.use(express.json())
@@ -12,6 +16,8 @@ app.use(fileUpload({
     tempFileDir: './upload'
 }))
 app.use(cors())
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // ROUTES
 app.use(postRoutes);
