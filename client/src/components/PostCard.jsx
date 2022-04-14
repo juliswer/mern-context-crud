cff9581359482184fd0c79325ede2666f29fcaba
juliswer <julianswer36@gmail.com>
2022-04-14 12:54:23 -0300

    deleteFunction

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,35 +1,50 @@
 import toast from "react-hot-toast";
-import {usePosts} from '../context/postContext'
-import {useNavigate} from 'react-router-dom';
+import { usePosts } from "../context/postContext";
+import { useNavigate } from "react-router-dom";
 
 function PostCard({ post }) {
-
-  const {deletePost} = usePosts();
+  const { deletePost } = usePosts();
   const navigate = useNavigate();
 
   const handleDelete = async (id) => {
-    toast((t) => (
-      <div>
-        <p className="text-white">Are you sure you want to delete this post? <strong>{id}</strong></p>
+    toast(
+      (t) => (
         <div>
-          <button className="bg-red-500 hover:bg-red-400 px-3 py-2 text-sm text-white rounded-sm mx-2" onClick={() => {
-            deletePost(id);
-            toast.dismiss(t.id)
-          }}>Delete</button>
-          <button className="bg-slate-400 hover:bg-slate-500 px-3 py-2 text-white rounded-sm mx-2" onClick={() => toast.dismiss(t.id)}>
-            Cancel
-          </button>
+          <p className="text-white">
+            Are you sure you want to delete this post? <strong>{id}</strong>
+          </p>
+          <div>
+            <button
+              className="bg-red-500 hover:bg-red-400 px-3 py-2 text-sm text-white rounded-sm mx-2"
+              onClick={() => {
+                deletePost(id);
+                toast.dismiss(t.id);
+              }}
+            >
+              Delete
+            </button>
+            <button
+              className="bg-slate-400 hover:bg-slate-500 px-3 py-2 text-white rounded-sm mx-2"
+              onClick={() => toast.dismiss(t.id)}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
-    ), {
-      style: {
-        background: '#202020'
+      ),
+      {
+        style: {
+          background: "#202020",
+        },
       }
-    });
+    );
   };
 
   return (
-    <div className="bg-zinc-800 text-white rounded-md shadow-black hover:bg-zinc-700 hover:cursor-pointer" onClick={() => navigate(`/posts/${post._id}`)}>
+    <div
+      className="bg-zinc-800 text-white rounded-md shadow-black hover:bg-zinc-700 hover:cursor-pointer"
+      onClick={() => navigate(`/posts/${post._id}`)}
+    >
       <div className="px-4 py-7">
         <div className="flex justify-between">
           <p>{post.title}</p>
